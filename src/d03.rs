01455f98aec0dcc155f9bb0f804f2c3ccc4c7a9b
Maksudul Haque <saad.mk112@gmail.com>
2022-12-03 17:41:20 +0000

    Improve day 3 part 1

--- src/d03.rs
+++ src/d03.rs
@@ -2,20 +2,15 @@ pub fn part1(input: String) -> i32 {
     input
         .lines()
         .map(|x| {
-            let mut total = 0;
             let (first, second) = x.split_at(x.len() / 2);
-            for s in first.chars() {
-                if second.contains(s) {
-                    if s.is_uppercase() {
-                        total += s as i32 - 64 + 26;
-                        break;
-                    } else {
-                        total += s as i32 - 96;
-                        break;
-                    }
-                }
+            first.chars().find(|b| second.contains(*b)).unwrap()
+        })
+        .map(|x| {
+            if x.is_uppercase() {
+                x as i32 - 64 + 26
+            } else {
+                x as i32 - 96
             }
-            total
         })
         .sum()
 }
