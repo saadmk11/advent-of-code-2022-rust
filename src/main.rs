e01726db8e91107d3e588986404fd39e153bbad6
Maksudul Haque <saad.mk112@gmail.com>
2022-12-05 11:14:48 +0000

    Add new script

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,23 @@ use helpers::read_file;
 
 mod helpers;
 
-mod d01;
-mod d02;
-mod d03;
-mod d04;
+mod d1;
+mod d2;
+mod d3;
+mod d4;
 
 fn main() {
     println!("Advent of Code 2022!");
     // Day 01
-    assert_eq!(74198, d01::part1(read_file(1)));
-    assert_eq!(209914, d01::part2(read_file(1)));
+    assert_eq!(74198, d1::part1(read_file(1)));
+    assert_eq!(209914, d1::part2(read_file(1)));
     // Day 02
-    assert_eq!(11873, d02::part1(read_file(2)));
-    assert_eq!(12014, d02::part2(read_file(2)));
+    assert_eq!(11873, d2::part1(read_file(2)));
+    assert_eq!(12014, d2::part2(read_file(2)));
     // Day 03
-    assert_eq!(7581, d03::part1(read_file(3)));
-    assert_eq!(2525, d03::part2(read_file(3)));
+    assert_eq!(7581, d3::part1(read_file(3)));
+    assert_eq!(2525, d3::part2(read_file(3)));
     // Day 04
-    assert_eq!(530, d04::part1(read_file(4)));
-    assert_eq!(903, d04::part2(read_file(4)));
+    assert_eq!(530, d4::part1(read_file(4)));
+    assert_eq!(903, d4::part2(read_file(4)));
 }
